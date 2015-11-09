679fcff1a7661f4207ab748c1bf377dbfccfbaf7
slenderballa <gswe4645@740s-pl-132.local>
2015-11-09 09:26:25 -0700

    Finished template for GUI

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -6,19 +6,31 @@ import gui.controller.GuiAppController;
 public class GUIFrame extends JFrame
 {
 	private GuiAppController baseController;
+	private GUIPanel basePanel;
 	
 	public GUIFrame(GuiAppController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new GUIPanel(baseController);
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
-		this.setSize(800, 1200);
+		this.setContentPane(basePanel);; //Must be first line of set up Frame!
+		this.setSize(800, 1200);// Find aod size for the app.
 		JFrame frame = new JFrame("Garret is Amazing");
-		this.setName("Poop");
-		this.setVisible(true);
+		
+		this.setVisible(true); //Must be last line of set upFrame!
 	}
+	
+	public GuiAppController getBaseController()
+	{
+		return baseController;
+	}
+	
+	
+	
+	
 	
 }
